d132b6197b7c8aa0004b371f6f3ac3c57c41ffa6
TANG ZHIXIONG <zhixiong@momenta.ai>
2023-12-04 23:56:27 +0800

    good

--- src/pointcloud_sax_reader/__main__.py
+++ src/pointcloud_sax_reader/__main__.py
@@ -1,10 +1,25 @@
 from __future__ import annotations
 
 import sys
+import time
 
 from . import bbox3d_of_pointcloud
 
 if __name__ == "__main__":
     assert len(sys.argv) > 1, f"usage:\n\t{sys.argv[0]} path/to/file.pcd"
-    bbox = bbox3d_of_pointcloud(sys.argv[1])
-    print("bbox:", bbox)  # noqa: T201
+    path = sys.argv[1]
+    tic = time.time()
+    bbox = bbox3d_of_pointcloud(path)
+    secs = time.time() - tic
+    print(f"bbox: {bbox} (ours, time: {secs:.6f})")  # noqa: T201
+
+    try:
+        import open3d as o3d
+
+        tic = time.time()
+        pcd = o3d.io.read_point_cloud(path)
+        aabb = pcd.get_axis_aligned_bounding_box()
+        secs = time.time() - tic
+        print(f"bbox: {aabb.min_bound.tolist()}, {aabb.max_bound.tolist()} (open3d, time: {secs:.6f})")  # noqa: T201
+    except ImportError:
+        pass
