1e1dc15918092352ddff51930f213d863aef33ca
TANG ZhiXiong <district10@users.noreply.github.com>
2023-12-05 22:46:18 +0800

    init version, ~6x speed up against open3d (#3)
    * clean
    
    * ready
    
    * add https://github.com/isl-org/Open3D/raw/2a11e0e40d5f9db4c80d9b6543e455d70a368822/cpp/open3d/io/file_format/FilePCD.cpp
    
    * add readPLY from libigl
    
    * add liblzf
    
    * lint code
    
    * remove cs
    
    * add tinyply
    
    * lint tinyply
    
    * sax reader
    
    * not good
    
    * not so good
    
    * good
    
    * ok
    
    * ready?
    
    * good
    
    * good
    
    * fix
    
    * ready
    
    * skip for loop
    
    * good
    
    * fix windows build, add fPIC
    
    * clean

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,24 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "pointcloud_sax_reader.hpp"
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
 int add(int i, int j) {
-    return i + j;
+  return i + j;
 }
 
 namespace py = pybind11;
+using namespace pybind11::literals;
 
 PYBIND11_MODULE(_core, m) {
-    m.doc() = R"pbdoc(
+  m.doc() = R"pbdoc(
         Pybind11 example plugin
         -----------------------
 
-        .. currentmodule:: scikit_build_example
+        .. currentmodule:: pointcloud_sax_reader
 
         .. autosummary::
            :toctree: _generate
@@ -23,21 +27,27 @@ PYBIND11_MODULE(_core, m) {
            subtract
     )pbdoc";
 
-    m.def("add", &add, R"pbdoc(
+  m.def("add", &add, R"pbdoc(
         Add two numbers
 
         Some other explanation about the add function.
     )pbdoc");
 
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+  m.def(
+      "subtract", [](int i, int j) { return i - j; }, R"pbdoc(
         Subtract two numbers
 
         Some other explanation about the subtract function.
     )pbdoc");
 
+  m.def("bbox2d_of_pointcloud", &cubao::bbox2d_of_pointcloud, "path"_a,
+        py::call_guard<py::gil_scoped_release>());
+  m.def("bbox3d_of_pointcloud", &cubao::bbox3d_of_pointcloud, "path"_a,
+        py::call_guard<py::gil_scoped_release>());
+
 #ifdef VERSION_INFO
-    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+  m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
 #else
-    m.attr("__version__") = "dev";
+  m.attr("__version__") = "dev";
 #endif
 }
